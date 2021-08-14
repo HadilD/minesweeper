d9d00cc3661b338942987596ab60cb028b37afbf
HadilBader <hadil.bader2@students.alquds.edu>
2021-08-14 19:30:55 +0300

    Added UI

--- Minesweeper/task/src/App.js
+++ Minesweeper/task/src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="bomb.svg" className="App-logo" alt="bomb" />
-        <p>
-          Minesweeper is loading...
-        </p>
-      </header>
-    </div>
-  );
+function ControlPanel(props) {
+    return (
+        <div className="ControlPanel">
+            <div className="ControlPanelCounter">{props.counter}</div>
+            <button className="ControlPanelReset">🙂</button>
+            <div className="ControlPanelTimer">{Math.floor(props.timer / 60)}:{(props.timer % 60).toString().padStart(2, '0')}</div>
+        </div>
+    );
 }
+
+function Field() {
+    return (
+        <div className="Field">
+            {[...Array(9).keys()].map((_, i) =>
+                <div key={i} className="FieldRow">
+                    {[...Array(8).keys()].map((_, j) =>
+                        <div key={i + '-' + j} className="FieldCell cell" />
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
+
+function App() {
+    let counter = 10;
+    let timer = 0;
+
+    return (
+        <div className="App">
+            <p className="AppHeader">
+                Minesweeper <img src="bomb.svg" className="App-logo" alt="logo" />
+            </p>
+            <ControlPanel counter={counter} timer={timer} />
+            <Field />
+        </div>
+    );
+}
+
 export default App;
